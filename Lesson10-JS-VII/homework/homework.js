5a730e87657ffd6cc71c379719accf437c60b1e0
Nicky <pacoman757@gmail.com>
2018-04-25 14:10:33 -0700

    lesson10

--- Lesson10-JS-VII/homework/homework.js
+++ Lesson10-JS-VII/homework/homework.js
@@ -23,6 +23,15 @@ function cacheFunction(cb) {
   // if the function you return is invoked with 5 it would pass 5 to cb(5) and return 25
   // if the function you return is invoked again with 5 it will look on an object in the closure scope
   // and return 25 directly and will not invoke cb again
+  var storage = {};
+  return function(argument) {
+   if (storage[argument] === undefined) {
+      storage[argument] = cb(argument);
+      return storage[argument];
+   } else {
+     return storage[argument];
+   };
+  };
 }
 
 // Do not modify code below this line.
